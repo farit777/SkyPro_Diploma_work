c275e342ce7d5da0ca4fc57f628fe3825b03942c
farit777 <farit777@mail.ru>
2024-08-27 12:43:16 +0300

    auth_test.py

--- page/AuthPage.py
+++ page/AuthPage.py
@@ -3,10 +3,19 @@ from selenium.webdriver.remote.webdriver import WebDriver
 
 class AuthPage:
 
-    def __init__(self, drv):
-        self.__url = 'https://www.kinopoisk.ru'
+    def __init__(self, drv: WebDriver):
+        self.__url = 'https://passport.yandex.ru/auth/welcome'
+
+#https://passport.yandex.ru/auth/welcome?origin=kinopoisk&retpath=https%3A%2F%2Fsso.passport.yandex.ru%2Fpush%3Fretpath%3Dhttps%253A%252F%252Fwww.kinopoisk.ru%252F%253Futm_referrer%253Dyandex.by%26uuid%3D69f26db9-01fe-4ecc-ac97-6cfe05c15949
+
         self.__driver = drv
 
     def go(self):
         self.__driver.get(self.__url)
-        
+
+    def login_as(self, email: str, passsword: str):
+        #self.__driver.find_element(By.CSS_SELECTOR, "#user").send_keys(email)
+        #self.__driver.find_element(By.CSS_SELECTOR, "#login").click()
+
+        self.__driver.find_element(By.CSS_SELECTOR, "#passp-field-passwd").send_keys(passsword)
+        self.__driver.find_element(By.CSS_SELECTOR, "#passp:sign-in").click()
--- test/auth_test.py
+++ test/auth_test.py
@@ -5,8 +5,10 @@ from page.AuthPage import AuthPage
 from time import sleep
 
 
-def test_first(browser):
+def auth_test(browser):
     auth_page = AuthPage(browser)
-    auth_page.go()
+#    auth_page.go()
+
+    auth_page.login_as("skyproap@mail.ru", "skypro_ap")
 
     sleep(5)
