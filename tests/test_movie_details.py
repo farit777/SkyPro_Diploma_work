d0069a565d70f99f05639c93a33d8b2f74ab8e51
farit777 <farit777@mail.ru>
2024-08-27 23:14:07 +0300

    Правка доступа

--- tests/test_movie_details.py
+++ tests/test_movie_details.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 import pytest
 from pages.SearchPage import SearchPage
 from pages.MoviePage import MoviePage
