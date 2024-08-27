b00df02ad26c70bb2f5a4d051b20a301e30bee54
farit777 <farit777@mail.ru>
2024-08-27 13:32:58 +0300

    test_auth.py

--- test/test_auth.py
+++ test/test_auth.py
@@ -1,13 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from page.AuthPage import AuthPage
 from time import sleep
 
 
 def auth_test(browser):
     auth_page = AuthPage(browser)
-#    auth_page.go()
+
+    auth_page.go()
 
     auth_page.login_as("skyproap@mail.ru", "skypro_ap")
 
