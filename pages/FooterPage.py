d5a096f3fa2d37521ef933fd8e883593dacab36c
farit777 <farit777@mail.ru>
2024-08-28 01:37:02 +0300

    __init__.py

--- pages/FooterPage.py
+++ pages/FooterPage.py
@@ -3,7 +3,12 @@ from selenium.webdriver.common.by import By
 class FooterPage:
     def __init__(self, driver):
         self.driver = driver
-        self.footer_links = (By.CSS_SELECTOR, "footer a")
+        self.url = 'https://www.kinopoisk.ru/'
+        self.footer_links = (By.CSS_SELECTOR, 'a.footer__content-link')
 
+    def go(self):
+        self.driver.get(self.url)
+        
     def get_footer_links(self):
-        return self.driver.find_elements(*self.footer_links)
+        
+        return self.driver.find_elements(self.footer_links)
